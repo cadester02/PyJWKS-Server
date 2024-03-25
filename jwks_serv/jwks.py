f96792017f1d3918c23a295b71bf7ab08e9d04d8
p_picklelol <111909631+bloxycolafan@users.noreply.github.com>
2024-03-24 22:08:52 -0500

    added sqlite3

--- jwks_serv/jwks.py
+++ jwks_serv/jwks.py
@@ -1,7 +1,8 @@
 from datetime import datetime, timedelta
 from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
 from flask import Flask, request, jsonify
-import uuid, base64, json, jwt, os
+import uuid, base64, json, jwt, os, sqlite3
 
 # format for a jwk
 # kid uuid
@@ -17,6 +18,14 @@ public_keys = {}
 
 # generate the server through flask
 app = Flask('jwks_server')
+
+conn = sqlite3.connect('totally_not_my_privateKeys.db', check_same_thread=False)
+c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS keys(
+                kid INTEGER PRIMARY KEY AUTOINCREMENT,
+                key BLOB NOT NULL,
+                exp INTEGER NOT NULL
+            )''')
 
 # fill public keys with what is stored in public_keys.json
 def open_public_keys():
@@ -40,33 +49,69 @@ def write_public_keys():
     with open('public_keys.json', 'w') as file:
         json.dump(public_keys, file)
 
+def serialized_private_key(key):
+    return key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption
+    )
+
+def fill_EmptyDB():
+    conn = sqlite3.connect('totally_not_my_privateKeys.db')
+    c = conn.cursor()
+
+    c.execute("SELECT COUNT(*) FROM keys")
+    numRows = c.fetchone()[0]
+
+    # if there are no rows generate and expired and an unexpired entry
+    if numRows == 0:
+        exp_private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048
+        )
+
+        # make int because of the db
+        exp_expiry = int((datetime.utcnow() - timedelta(days=1)).timestamp())
+
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048
+        )
+
+        expiry = int((datetime.utcnow() + timedelta(days=1)).timestamp())
+
+        c.execute("INSERT INTO keys (key, exp) VALUES (?, ?)", serialized_private_key(exp_private_key).decode(), exp_expiry)
+        c.execute("INSERT INTO keys (key, exp) VALUES(?, ?)", serialized_private_key(private_key), expiry)
+
+        conn.commit()
+        conn.close()
+
+
 # process the get request
 @app.route('/.well-known/jwks.json')
 def jwks():
-    global public_keys
-
     # get current time
     cur_time = datetime.utcnow().timestamp()
 
-    # create a list of acceptable keys
-    keys = []
+    c.execute("SELECT key FROM keys WHERE exp >= ?", cur_time)
+    nonExpired_keys = []
+    for row in c.fetchall():
+        nonExpired_keys.append(serialization.load_pem_private_key(row[0].encode(), password=None))
 
-    # if the expiry is greater than the current time then it is valid and add it to keys
-    # e is always AQAB
-    for kid, key in public_keys.items():
-        expiry = key.get('exp', 0)
-        if expiry > cur_time:
-            keys.append({
-                'kid': kid,
-                'alg': key['alg'],
-                'kty': key['kty'],
-                'use': key['use'],
-                'n': key['n'],
-                'e': 'AQAB',
-                'exp': key['exp'],
-            })
+    jwks = []
+    for i, key in enumerate(nonExpired_keys):
+        jwks.append({
+            "kid": str(uuid.uuid4),
+            "alg": "RS256",
+            "kty": "RSA",
+            "use": "sig",
+            "n": base64.urlsafe_b64encode(key.public_key().public_numbers().n.to_bytes(256, byteorder='big')).decode().rstrip('='),
+            "e": "AQAB"
+        })
 
-    return jsonify({"keys": keys})
+    return jsonify(jwks)
+
+    
 
 # process the post request
 @app.route('/auth', methods=['POST'])
@@ -74,51 +119,26 @@ def auth():
     # check if it is expired
     isExpired = request.args.get('expired') == 'true'
 
-    # generate the private rsa key
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048,
-    )
+    fill_EmptyDB()
+    
+    curDate = datetime.utcnow().timestamp()
 
-    # get the public key
-    public_key = private_key.public_key()
-
-    # encode the public key in base 64 url
-    n_bytes = public_key.public_numbers().n.to_bytes(256, byteorder='big')
-    n_b64url = base64.urlsafe_b64encode(n_bytes).rstrip(b'=').decode()
-
-    # fill out the global variable public_keys
-    open_public_keys()
-
-    # generate kid as new uuid
-    kid = str(uuid.uuid4())
-
-    # generate expiry
     if isExpired:
-        expiry = (datetime.utcnow() - timedelta(days=1)).timestamp()
+        c.execute("SELECT key FROM keys WHERE exp < ?", curDate)
+        row = c.fetchone()
+        if row:
+            private_key = serialization.load_pem_private_key(row[0].encode(), password=None)
+            token = jwt.encode({'username': 'user', 'password': 'password'}, private_key, algorithm='RS256', headers={'kid': str(uuid.uuid4)})
+        else:
+            token = "Error key not found"
     else:
-        expiry = (datetime.utcnow() + timedelta(days=1)).timestamp()
-
-    # add new entry into public keys
-    # e is always AQAB
-    public_keys[kid] = {
-        'kid': kid,
-        'alg': 'RS256',
-        'kty': 'RSA',
-        'use': 'sig',
-        'n': n_b64url,
-        'e': 'AQAB',
-        'exp': expiry,
-    }
-
-    # write to the file
-    write_public_keys()
-
-    # store the result in a jwt token
-    if not isExpired:
-        token = jwt.encode({'username': 'user'}, private_key, headers = {'kid': kid, 'alg': 'RS256'})
-    else:
-        token = jwt.encode({'username': 'user', 'exp': expiry}, private_key, algorithm = 'RS256', headers = {'kid': kid})
+        c.execute("SELECT key FROM keys WHERE exp >= ?", curDate)
+        row = c.fetchone()
+        if row:
+            private_key = serialization.load_pem_private_key(row[0].encode(), password=None)
+            token = jwt.encode({'username': 'user', 'password': 'password'}, private_key, algorithm='RS256', headers={'kid': str(uuid.uuid4)})
+        else:
+            token = "Error key not found"
 
     return token
 
